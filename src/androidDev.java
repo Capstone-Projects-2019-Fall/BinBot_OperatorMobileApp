92973348cf0e6fc7c7d363ef9b21081eb6f7d7ad
Sean <SeanR1194@gmail.com>
2019-10-13 19:28:30 -0400

    Added androidDev doc

---
+++ src/androidDev.java
@@ -0,0 +1,53 @@
+package application;
+
+/**
+ * @author Kwamina M Thompson
+ * @version 1.0
+ * @since 2019-10-13
+ */
+
+public class androidDev {
+	
+	/**
+	 * <h1>OnJSONreceiver(Object JSON )</h1> this method method is will receive all JSON
+	 * objects and translate it to formats that can be read in java/android system
+	 *
+	 * @author Kwamina M Thompson
+	 * @version 1.0
+	 * @since 2019-10-13
+	 *
+	 */
+	
+	void OnJSONreciever(Object JSON) {
+	}
+
+	/**
+	 * <h1>TouchOnButton() </h1> This method gets called when a user touches the on button 
+	 * and sends packets to the server to turn on BinBot
+	 */
+	void TouchOnButton() {
+		
+	}
+	
+	/**
+	 * <h1>TouchOffButton() </h1>  This method gets called when the user touches the off 
+	 * button the screen and notifies the server to shutdown BinBot operations
+	 */
+	void TouchOffButton() {
+		
+	}
+	
+	/**
+	 * <h1> OnDisplay(Object img) </h1> this method takes in an image object that has been 
+	 * was processed by OnJSONreciever method then displays the image on the application's
+	 * screen.
+	 * @author Kwamina M Thompson
+	 * @version 1.0
+	 * @param image
+	 * @since 2019-10-13
+	 */
+	private void OnDisplay(Object img) {
+		
+	}
+
+}
