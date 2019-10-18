785acd1ba15d6a2fbd7eb7a1696119b594130c1b
Kwamina Thompson <tug82420@temple.edu>
2019-10-18 16:39:06 -0400

    Input Stream created

--- app/src/main/java/com/example/binbot_mobiledev/JSONreciever.java
+++ app/src/main/java/com/example/binbot_mobiledev/JSONreciever.java
@@ -2,13 +2,32 @@ package com.example.binbot_mobiledev;
 
 import android.os.AsyncTask;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 public class JSONreciever extends AsyncTask<Void,Void,Void> {
 
+    Socket socket;
 
     @Override
     protected Void doInBackground(Void... voids) {
 
-        
+        /*Get socket and pot number for connection*/
+        try
+        {
+            socket = new Socket("", Integer.parseInt(null));
+
+            InputStream inputStream = socket.getInputStream();
+
+            
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         return null;
     }
 }
