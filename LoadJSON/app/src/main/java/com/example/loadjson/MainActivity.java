4f67b72b03d5dcbd29c03b23a5c3ce73e0bf6346
Kwamina Thompson <tug82420@temple.edu>
2019-10-19 20:46:24 -0400

    New Class created to extract data

---
+++ LoadJSON/app/src/main/java/com/example/loadjson/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.loadjson;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button download ;
+    ImageView imageView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        download = findViewById(R.id.getImage);
+        imageView = findViewById(R.id.imageViewer);
+
+        download.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
+    }
+}
---
+++ LoadJSON/app/src/main/java/com/example/loadjson/getData.java
@@ -0,0 +1,4 @@
+package com.example.loadjson;
+
+public class getData {
+}
