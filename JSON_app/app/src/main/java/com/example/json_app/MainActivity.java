51129ae03f1632214a22e713d2d261f7829ced97
Kwamina Thompson <tug82420@temple.edu>
2019-10-24 15:53:24 -0400

    JSON_app MainActivity set to Landscape Only

--- JSON_app/app/src/main/java/com/example/json_app/MainActivity.java
+++ JSON_app/app/src/main/java/com/example/json_app/MainActivity.java
@@ -36,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
         start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                jsonParser();
             }
         });
 
