884f8d64f5ccfbee6ca5b19d64a8fb216965be93
Kwamina Thompson <tug82420@temple.edu>
2019-10-31 18:48:48 -0400

    MessageSender Class created to receive a string from the screen of app and relay to server

--- ServerDev/app/src/main/java/com/example/server/MessageSender.java
+++ ServerDev/app/src/main/java/com/example/server/MessageSender.java
@@ -1,4 +1,40 @@
 package com.example.server;
 
-public class MessageSender {
+import android.os.AsyncTask;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class MessageSender extends AsyncTask <String,Void,Void>{
+
+
+    final  String ip = " 192.168.56.1";
+    Socket socket;
+    DataOutputStream dataOutputStream;
+    PrintWriter printWriter;
+
+    @Override
+    protected Void doInBackground(String... voids) {
+
+        String message = voids [0];
+
+        try
+        {
+            socket = new Socket(ip, 9800);
+            printWriter = new PrintWriter(socket.getOutputStream());
+            printWriter.write(message);
+            printWriter.flush();
+            socket.close();
+
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
 }
