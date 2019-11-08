b8ee3f3265dea5d1ada8c3265e92cc132a8bbb49
Kwamina Thompson <tug82420@temple.edu>
2019-11-07 22:37:54 -0500

    background asyncTask begun

--- app/src/main/java/edu/temple/BinBot_OperatorMobileApp/MainActivity.java
+++ app/src/main/java/edu/temple/BinBot_OperatorMobileApp/MainActivity.java
@@ -1,9 +1,14 @@
 package edu.temple.BinBot_OperatorMobileApp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import edu.temple.BinBot_OperatorMobileApp.connections.ServerConnection;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,5 +23,32 @@ public class MainActivity extends AppCompatActivity {
 
         start = findViewById(R.id.start_button);
         stop = findViewById(R.id.stop_button);
+
+
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
+    static class MakeConnection extends AsyncTask<Void,Void,Void>{
+
+        static final String IP = "192.168.56.1";
+        static final int PORT = 7002;
+        private static ServerConnection serverConnection;
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            try {
+                serverConnection = new ServerConnection(IP,PORT);
+            } catch (IOException e) {
+
+            }
+
+            return null;
+        }
+    }
+
+
 }
